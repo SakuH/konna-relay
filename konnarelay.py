9a6bdd0fb5882dbd9221998618356b02cae81bc3
SakuH <t7humi01@students.oamk.fi>
2018-12-10 09:44:45 -0200

    viimeinen versio

--- konnarelay.py
+++ konnarelay.py
@@ -1,38 +1,25 @@
 #!/usr/bin/env python
+
 import time
 import serial
 import mysql.connector
 
-##Konnarelay ver 0.5
+##Konnarelay ver 1.0
 ##by Saku Huuha
 
-##cnx = mysql.connector.connect(user='t7hani00', password='NEwttgtub7h3FHYq',
-##                              host='mysli.oamk.fi',
-##                              database='opisk_t7hani00')
-##print(cnx)
-##dbcursor = cnx.cursor()
-##
-##
-##ser = serial.Serial(
-##    port='/dev/ttyACM0',
-##    baudrate = 9600,
-##    parity=serial.PARITY_NONE,
-##    stopbits=serial.STOPBITS_ONE,
-##    bytesize=serial.EIGHTBITS,
-##    timeout=10
-##    )
-
+##While-silmukka ja try - except-rakenne lisaa luotettavuutta
 ser = None
 cnx = None
 while True:
     try:
+##        Luodaan yhteys tietokantaan, jos yhteytta ei ole
         if cnx == None:
             cnx = mysql.connector.connect(user='t7hani00', password='NEwttgtub7h3FHYq',
                                           host='mysli.oamk.fi',
                                           database='opisk_t7hani00')
             print(cnx)
             dbcursor = cnx.cursor()
-
+##Luodaan sarjaporttiyhteys tiettyyn USB-porttiin(alhaalla vasemmalla Raspissa)
         if ser == None:
             ser = serial.Serial(
                 port='/dev/ttyACM0',
@@ -44,19 +31,14 @@ while True:
                 )
             
 
-
-        #dbcursor.execute("SHOW TABLES")
-        #for x in dbcursor:
-        #   print(x)
-
+##Luetaan rivi sarjaportilta
         x=ser.readline()
-    #    ser.flushInput()
         print x
-    #    x = "25.2 120.2 66.6 17.9"
+##Jaetaan lampotila, kulma x ja kulma y listaan
         konnanarvot = x.split(", ")
         if len(konnanarvot)==3:
-            sql = "UPDATE sensori_data SET lampotila = %s, kulma_x = %s, kulma_y = %s WHERE data_id = %s"
-    #       val = (x, "900.0")
+##Tietokantaoperaatiot
+            sql = "UPDATE sensori_data SET lampotila = %s, kulma_x = %s, kulma_y = %s, nalka = nalka + 1 WHERE data_id = %s"
             val = (konnanarvot[0], konnanarvot[1], konnanarvot[2], "1")
             dbcursor.execute(sql, val)
             cnx.commit()
